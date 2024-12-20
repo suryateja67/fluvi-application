508c6db4b46962accfd33917546cc7f672cc69f8
suryafa <144129975+suryafa@users.noreply.github.com>
2024-12-20 16:08:44 +0530

    Update joke_crud.py
    Added function to update joke

--- app/crud/joke_crud.py
+++ app/crud/joke_crud.py
@@ -14,7 +14,15 @@ async def get_all_jokes():
     return await Joke.find_all().to_list() 
 
 async def get_joke_by_id(joke_id: str):
-    return await Joke.find_one({"_id": joke_id})  
+    return await Joke.find_one({"_id": joke_id})
+
+async def update_joke(joke_id: str, new_joke_text: str):
+    joke = await Joke.find_one({"_id": joke_id})
+    if joke:
+        joke.joke = new_joke_text
+        await joke.save()
+        return True
+    return False
 
 async def delete_joke(joke_id: str) -> bool:
     joke = await Joke.find_one({"_id": joke_id})
